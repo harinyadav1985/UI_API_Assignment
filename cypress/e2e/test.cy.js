c03bf60541f95c13ccae748f8a1220095db8292f
Hari Yadav <anilypb@gmail.com>
2023-04-28 14:15:22 +0530

    new commit

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -8,10 +8,13 @@ it("verify login", () => {
     "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
   );
   cy.get("[name = username]").type("Admin");
-  cy.get("[name = password]").type("admin123");
+  cy.get("[name = username]").invoke("val", 25).trigger("change");
+  cy.get("[name = username]").should("have.value", "25");
+
+  /* cy.get("[name = password]").type("admin123");
   cy.get("[type=submit]").click();
   cy.title().should("contain", "OrangeHRM");
   cy.contains("Admin").click();
   cy.get("[alt=`profile picture`]").click();
-  cy.contains("Logout");
+  cy.contains("Logout"); */
 });
